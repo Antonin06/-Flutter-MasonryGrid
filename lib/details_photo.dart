dffaa444d49e7aebf031f78240d2596b7c3a27ec
Antonin06 <avon.antonin@gmail.com>
2020-10-15 16:58:25 +0200

    PageView Test

--- lib/details_photo.dart
+++ lib/details_photo.dart
@@ -1,8 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class DetailsPhoto extends StatelessWidget {
+  List<ImageDetails> _images = [
+    ImageDetails(
+        imagePath: "https://source.unsplash.com/VFRTXGw1VjU",
+        title: "Toto",
+        date: "14 Octobre 2020",
+        location: "Roanne",
+        comment: "Super Week-end à Rome!"),
+    ImageDetails(
+        imagePath: "https://source.unsplash.com/7ybKmhDTcz0",
+        title: "Toto",
+        date: "14 Octobre 2020",
+        location: "Roanne",
+        comment: "Super Week-end à Rome!"),
+    ImageDetails(
+        imagePath: "https://source.unsplash.com/S0hS0HfH_B8",
+        title: "Toto",
+        date: "14 Octobre 2020",
+        location: "Roanne",
+        comment: "Super Week-end à Rome!"),
+    ImageDetails(
+        imagePath: "https://source.unsplash.com/8CGT0Kq6K3k",
+        title: "Toto",
+        date: "14 Octobre 2020",
+        location: "Roanne",
+        comment: "Super Week-end à Rome!"),
+    ImageDetails(
+        imagePath: "https://source.unsplash.com/BchXuilibLA",
+        title: "Toto",
+        date: "14 Octobre 2020",
+        location: "Roanne",
+        comment: "Super Week-end à Rome!"),
+    ImageDetails(
+        imagePath: "https://source.unsplash.com/2N-kwvSeU5U",
+        title: "Toto",
+        date: "14 Octobre 2020",
+        location: "Roanne",
+        comment: "Super Week-end à Rome!"),
+    ImageDetails(
+        imagePath: "https://source.unsplash.com/tHXX4fl3-ms",
+        title: "Toto",
+        date: "14 Octobre 2020",
+        location: "Roanne",
+        comment: "Super Week-end à Rome!"),
+    ImageDetails(
+        imagePath: "https://source.unsplash.com/cjBLfrjE-XU",
+        title: "Toto",
+        date: "14 Octobre 2020",
+        location: "Roanne",
+        comment: "Super Week-end à Rome!"),
+    ImageDetails(
+        imagePath: "https://source.unsplash.com/RH0QUHYPeW4",
+        title: "Toto",
+        date: "14 Octobre 2020",
+        location: "Roanne",
+        comment: "Super Week-end à Rome!"),
+    ImageDetails(
+        imagePath: "https://source.unsplash.com/kaEhf0eZme8",
+        title: "Toto",
+        date: "14 Octobre 2020",
+        location: "Roanne",
+        comment: "Super Week-end à Rome!"),
+  ];
   final String imagePath;
   final String title;
   final String date;
@@ -10,147 +75,53 @@ class DetailsPhoto extends StatelessWidget {
   final String comment;
   final int index;
   DetailsPhoto(
-    {
-    this.imagePath,
-    this.title,
-    this.date,
-    this.location,
-    this.comment,
-    this.index
-    }
-  );
+      {this.imagePath,
+      this.title,
+      this.date,
+      this.location,
+      this.comment,
+      this.index});
+
+  PageController pageController = PageController(initialPage: 0);
 
   @override
   Widget build(BuildContext context) {
+    // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('$title'),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              Container(
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(12),
-                  child: Image.network(
-                    imagePath,
-                  ),
+          backgroundColor: Colors.black,
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              color: Colors.white,
+              onPressed: () {
+                Get.back();
+              })),
+      body: PageView.builder(
+          controller: pageController,
+          pageSnapping: true,
+          itemCount: _images.length,
+          itemBuilder: (context, int index) {
+            return Container(
+              child: Center(
+                child: FadeInImage.memoryNetwork(
+                  placeholder: kTransparentImage,
+                  image: _images[index].imagePath,
+                  fit: BoxFit.cover,
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(top: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      padding: EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                          // color: Colors.red,
-                          borderRadius: BorderRadius.circular(10)),
-                      width: MediaQuery.of(context).size.width,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Center(
-                            child: Text(
-                              title,
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 22,
-                                fontStyle: FontStyle.normal,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Center(
-                            child: Text(
-                              comment,
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontStyle: FontStyle.italic,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Row(
-                                children: [
-                                  Icon(Icons.location_on,
-                                      color: Colors.red, size: 35),
-                                  Text(
-                                    location,
-                                    style: TextStyle(
-                                        fontSize: 17,
-                                        fontStyle: FontStyle.italic,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                date,
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  fontStyle: FontStyle.italic,
-                                  fontWeight: FontWeight.normal,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        IconButton(
-                            icon: FaIcon(
-                              FontAwesomeIcons.facebook,
-                              size: 30,
-                              color: HexColor("#0674E7"),
-                            ),
-                            onPressed: () {}),
-                        IconButton(
-                            icon: FaIcon(
-                              FontAwesomeIcons.instagram,
-                              size: 30,
-                              color: HexColor("#C13584"),
-                            ),
-                            onPressed: () {}),
-                        IconButton(
-                            icon: FaIcon(
-                              FontAwesomeIcons.twitter,
-                              size: 30,
-                              color: HexColor("#1da1f2"),
-                            ),
-                            onPressed: () {}),
-                        IconButton(
-                            icon: FaIcon(
-                              FontAwesomeIcons.userCircle,
-                              size: 30,
-                              color: Colors.orange,
-                            ),
-                            onPressed: () {}),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+            );
+          }),
     );
   }
+}
+
+class ImageDetails {
+  final String imagePath;
+  final String title;
+  final String date;
+  final String location;
+  final String comment;
+  ImageDetails(
+      {this.imagePath, this.title, this.date, this.location, this.comment});
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:memolidays_souvenir/settings.dart';
 import 'package:memolidays_souvenir/souvenir_page.dart';
 
 void main() {
   runApp(MyApp());
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 }
 
 class MyApp extends StatelessWidget {
@@ -13,17 +15,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData( 
-        primarySwatch: Colors.orange, 
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false,
       home: SouvenirPage(),
-      getPages: [
-        GetPage(name: '/details', page: () => Settings())
-      ],
+      getPages: [GetPage(name: '/details', page: () => Settings())],
     );
   }
 }
-
-
